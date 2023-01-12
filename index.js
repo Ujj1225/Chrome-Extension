5ea23f6b1cf6b8063134e9c50d48c1bee88b697b
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-13 05:40:11 +0545

    Used addEventListener here

---
+++ index.js
@@ -0,0 +1,6 @@
+let inputBtn = document.querySelector("#input-btn")
+
+// More professional way of using onclick
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from add event listener.")
+})
