cd0cda0c7bcbaeab1e1d33bdefb1d7430ad88e3c
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-14 06:07:58 +0545

    Getting input using .value method

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let inputBtn = document.querySelector("#input-btn")
+let mylead = [];
+// const is used for storing constant
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn")
 
 // More professional way of using onclick
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from add event listener.")
+    mylead.push(inputEl.value);
+    console.log(mylead);
 })
