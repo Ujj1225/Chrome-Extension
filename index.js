f919c2456c839f54a738c6c9bec35f42cde32cd9
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-15 09:52:21 +0545

    Added for loop and unordered loop

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let mylead = [];
 // const is used for storing constant
 const inputEl = document.querySelector("#input-el");
-const inputBtn = document.querySelector("#input-btn")
-
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el")
+console.log(ulEl);
 // More professional way of using onclick
 inputBtn.addEventListener("click", function(){
     mylead.push(inputEl.value);
     console.log(mylead);
 })
+mylead = ["www.awesomeleads.com", "www.epicleads.com", "www.leadkun.com"];
+for (let i=0; i<mylead.length; i++)
+{
+    ulEl.textContent += mylead[i] + " ";
+}
