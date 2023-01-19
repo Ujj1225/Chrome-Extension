a0f6abab674f162d9c23c85cc7b57d9c460664b9
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-19 06:23:53 +0545

    Learnt parsing and stringify with JSON

--- index.js
+++ index.js
@@ -14,11 +14,36 @@
 // }
 
 
-let mylead = [];
+// let mylead = [];
+
+let myleads = `["www.awesomelead.com"]`;
+myleads = JSON.parse(myleads);
+myleads.push("www.leadkun.com");
+myleads = JSON.stringify(myleads);
+console.log(typeof(myleads));
 // const is used for storing constant
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el")
+
+// Local Storage
+// Adding key value pair in local Storage
+localStorage.setItem("myleads", "www.example.com");
+
+// logging out the value of local Storage in Console
+console.log(localStorage.getItem("myleads"));
+
+// Clearing out local storage 
+localStorage.clear();
+
+// There are two methods for arrays to string and string to arrays 
+
+// One is JSON.stringify and the other is JSON.parse
+
+// JSON.parse le string lai array ma lagcha
+
+// JSON.stringify le array lai string ma lagcha. Only strings are accepted as key value pairs in JSON
+
 
 // More professional way of using onclick
 inputBtn.addEventListener("click", function(){
