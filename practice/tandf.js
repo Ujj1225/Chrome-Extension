94bff077a047d133ad90fb1e1c3f3b013ccf5715
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-19 07:19:20 +0545

    Learnt about truthy and falsy values

---
+++ practice/tandf.js
@@ -0,0 +1,18 @@
+// Here we will get to know about the truthy and falsy values
+const credits = 0;
+
+if(credits)
+{
+    console.log("Let's play")
+}
+else{
+    console.log("Sorry, you have no credits left")
+}
+
+// For falsy values remember these data types
+// false
+// 0
+// null -> how as a developer we signify emptiness
+// undefined -> how Js signifies emptiness
+// NaN
+// ""
