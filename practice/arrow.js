daf7d532095c595cb7c16cb60a438ae75ee7490f
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-31 05:31:21 +0545

    Arrow function with and without parameter

--- practice/arrow.js
+++ practice/arrow.js
@@ -1,3 +1,7 @@
-// This is one way to use arrow function. 
+// Arrow function with parameters
 const add = (a,b)=> a+b;
 console.log(add(4,5));
+
+// Arrow function without parameters
+const greet = ()=> "Hello";
+console.log(greet());
